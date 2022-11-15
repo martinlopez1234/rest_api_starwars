dd0823595f90dfd9d139094801b154934ba393f4
martinlopez1234 <57772762+martinlopez1234@users.noreply.github.com>
2022-11-15 13:15:20 -0300

    cambios

--- src/models.py
+++ src/models.py
@@ -1,12 +1,45 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
+Base = declarative_base()
+class User(Base):
+    __tablename__ = 'usuarios'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False, unique= True)
+    password = Column(String(100), nullable=False)
+    apellido = Column(String(250), nullable= False)
+    email = Column(String (100), nullable=False, unique = True)
+    fecha_de_subscripción = Column(Integer,nullable=False)
+    
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    class Favoritos(Base):
+        __tablename__ = 'favoritos'
+        id_usuario = Column(Integer, ForeignKey('usuarios.id'), nullable= False, primary_key=True)
+        id_favoritos = Column(Integer, ForeignKey('personajes.id'), ForeignKey('planetas.id'))
+        fav_name = Column(String(250), ForeignKey('personajes.name'), ForeignKey('planetas.name'))
+        fav_tema = Column(String(250), ForeignKey('personajes.__tablename__'), ForeignKey('planetas.__tablename__'))
+
+
+class Personajes(Base):
+    __tablename__ = 'personajes'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(250))
+    hair_color = Column(String(250))
+    eye_color = Column(String(250))
+ 
+
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    population = Column(Integer)
+    terrain = Column(String(250))
+    climate = Column(String(250))
 
     def __repr__(self):
         return '<User %r>' % self.username
