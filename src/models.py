4a06929489ae0f421cddeae506caf513a3827921
martinlopez1234 <57772762+martinlopez1234@users.noreply.github.com>
2022-12-17 14:20:32 -0300

    cambios

--- src/models.py
+++ src/models.py
@@ -5,48 +5,79 @@ from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 Base = declarative_base()
-class User(Base):
-    __tablename__ = 'usuarios'
-    id = Column(Integer, primary_key=True)
-    username = Column(String(250), nullable=False, unique= True)
-    password = Column(String(100), nullable=False)
-    apellido = Column(String(250), nullable= False)
-    email = Column(String (100), nullable=False, unique = True)
-    fecha_de_subscripción = Column(Integer,nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     
-
-    class Favoritos(Base):
-        __tablename__ = 'favoritos'
-        id_usuario = Column(Integer, ForeignKey('usuarios.id'), nullable= False, primary_key=True)
-        id_favoritos = Column(Integer, ForeignKey('personajes.id'), ForeignKey('planetas.id'))
-        fav_name = Column(String(250), ForeignKey('personajes.name'), ForeignKey('planetas.name'))
-        fav_tema = Column(String(250), ForeignKey('personajes.__tablename__'), ForeignKey('planetas.__tablename__'))
-
-
-class Personajes(Base):
-    __tablename__ = 'personajes'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    gender = Column(String(250))
-    hair_color = Column(String(250))
-    eye_color = Column(String(250))
- 
-
-
-class Planetas(Base):
-    __tablename__ = 'planetas'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    population = Column(Integer)
-    terrain = Column(String(250))
-    climate = Column(String(250))
-
     def __repr__(self):
         return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "email": self.email            
             # do not serialize the password, its a security breach
         }
+
+  
+class Characters(db.Model):    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    birth_year = db.Column(db.String(100), unique=False, nullable=False)
+    gender = db.Column(db.String(100), unique=False, nullable=False)
+    hair_color = db.Column(db.String(100), unique=False, nullable=False)
+    eye_color = db.Column(db.String(100), unique=False, nullable=False)
+    Favorite_Characters = db.relationship('Favorite_Characters', lazy=True)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    population = db.Column(db.Integer)
+    terrain = db.Column(db.String(100), unique=False, nullable=False)
+    Favorite_Planets = db.relationship('Favorite_Planets', lazy=True)    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "terrain": self.terrain
+       }
+
+class Favorite_Characters(db.Model):      
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    characters_id = db.Column(db.Integer, db.ForeignKey('characters.id'))    
+
+    def serialize(self):
+        return{
+            "id" : self.id,
+            "user_id": self.user_id,            
+            "characters_id" : self.characters_id            
+        }
+
+class Favorite_Planets(db.Model):      
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))    
+
+    def serialize(self):
+        return{
+            "id" : self.id,
+            "user_id" : self.user_id,
+            "planet_id" : self.planet_id
+        }
+
+ 
